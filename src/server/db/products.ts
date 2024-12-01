7b5129d186445ac8161f0bbb243534feaff48f75
Ojasvi <ojasvidoye@gmail.com>
2024-12-01 20:07:07 +0530

    caching

--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -1,14 +1,24 @@
 import { db } from "@/drizzle/db";
 import { ProductCustomizationTable, ProductTable } from "@/drizzle/schema";
+import {
+  CACHE_TAGS,
+  dbCache,
+  getUserTag,
+  revalidateDbCache,
+} from "@/lib/cache";
 import { and, eq } from "drizzle-orm";
 
-export function getProducts(userId: string, { limit }: { limit?: number }) {
-  return db.query.ProductTable.findMany({
-    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
-    orderBy: ({ createdAt }, { desc }) => desc(createdAt),
-    limit,
+export function getProducts(
+  userId: string,
+  { limit }: { limit?: number } = {}
+) {
+  const cacheFn = dbCache(getProductsInternal, {
+    tags: [getUserTag(userId, CACHE_TAGS.products)],
   });
+
+  return cacheFn(userId, { limit });
 }
+
 export async function createProduct(data: typeof ProductTable.$inferInsert) {
   const [newProduct] = await db
     .insert(ProductTable)
@@ -28,9 +38,14 @@ export async function createProduct(data: typeof ProductTable.$inferInsert) {
     await db.delete(ProductTable).where(eq(ProductTable.id, newProduct.id));
   }
 
+  revalidateDbCache({
+    tag: CACHE_TAGS.products,
+    userId: newProduct.userId,
+    id: newProduct.id,
+  });
+
   return newProduct;
 }
-
 export async function deleteProduct({
   id,
   userId,
@@ -42,8 +57,24 @@ export async function deleteProduct({
     .delete(ProductTable)
     .where(and(eq(ProductTable.id, id), eq(ProductTable.clerkUserId, userId)))
     .execute();
+  if (result.rowCount > 0) {
+    revalidateDbCache({
+      tag: CACHE_TAGS.products,
+      userId,
+      id,
+    });
+  }
 
-  const affectedRows = result.rowCount || result.affectedRows || 0;
+  const affectedRows = result.rowCount || 0;
+  //  const affectedRows = result.rowCount || result.affectedRows || 0;
 
   return affectedRows > 0;
+}
+
+function getProductsInternal(userId: string, { limit }: { limit?: number }) {
+  return db.query.ProductTable.findMany({
+    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
+    orderBy: ({ createdAt }, { desc }) => desc(createdAt),
+    limit,
+  });
 }
--- src/server/db/subscription.ts
+++ src/server/db/subscription.ts
@@ -1,10 +1,28 @@
 import { db } from "@/drizzle/db";
 import { UserSubscriptionTable } from "@/drizzle/schema";
+import { CACHE_TAGS, revalidateDbCache } from "@/lib/cache";
 
-export function createUserSubscription(
+export async function createUserSubscription(
   data: typeof UserSubscriptionTable.$inferInsert
 ) {
-  return db.insert(UserSubscriptionTable).values(data).onConflictDoNothing({
-    target: UserSubscriptionTable.clerkUserId,
-  });
+  const [newSubscription] = await db
+    .insert(UserSubscriptionTable)
+    .values(data)
+    .onConflictDoNothing({
+      target: UserSubscriptionTable.clerkUserId,
+    })
+    .returning({
+      id: UserSubscriptionTable.id,
+      userId: UserSubscriptionTable.clerkUserId,
+    });
+
+  if (newSubscription != null) {
+    revalidateDbCache({
+      tag: CACHE_TAGS.subscription,
+      id: newSubscription.id,
+      userId: newSubscription.userId,
+    });
+  }
+
+  return newSubscription;
 }
